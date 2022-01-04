84085854a2abff8ccc0e2cffd66ae30cbb57a722
maicondf <maiconmssnet1@gmail.com>
2022-01-04 11:24:56 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Jogo Genius feito em HTML, CSS e JavaScript para o Bootcamp da Dio.
+
+### Foi aprimorado alguns recursos referente ao jogo. 
+
+### Adicionado botão Play/Reset
+
+### Adicionado na tela visualizador de Score
+
+### Adicionado plano de fundo usando linear-gradient
+
+### Adicionado recursos de css para melhoria do visual
+
+
+## 🚀 Let's code! 🚀
---
+++ script.js
@@ -0,0 +1,113 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+// 0 = verde
+// 1 = vermelho
+// 2 = amarelo
+// 3 = azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+// Criando ordem aleatórias de cores.
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for (let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+// Acende a próxima cor.
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+
+    setTimeout(() => {
+        element.classList.remove('selected');
+    },number);
+}
+
+// Faz a checagem se os botões são os mesmos da ordem gerada no jogo.
+let checkOrder = () => {
+    for (let i in clickedOrder) {
+        if (clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if (clickedOrder.length == order.length) {
+        score++;
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando o próximo nível!`)
+        nextLevel();
+        const btnScore = document.getElementById('Print');
+        btnScore.innerHTML = `Score: ${score}`;
+    }
+}
+
+// Funcão para o clique do usuário.
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected')
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected')
+        checkOrder();
+    },250)    
+}
+
+// função que retorna a cor.
+let createColorElement = (color) => {
+    if (color == 0) {
+        return green;
+    } else if (color == 1) {
+        return red;
+    } else if (color == 2) {
+        return yellow;
+    } else if (color == 3){
+        return blue;
+    }
+}
+
+//Função para o próximo nível do jogo.
+let nextLevel = () => {
+    shuffleOrder();
+}
+
+//Função game over.
+let gameOver = () => {
+    alert(`Pontuação: ${score}\nVocê perdeu o jogo!\nClique em OK para iniciar o jogo.`);
+    order = [];
+    clickedOrder = [];
+    score = 0;
+    
+    playGame();
+}
+
+//Função de começar o jogo.
+let playGame = () => {
+    const btnScore = document.getElementById('Print');
+    btnScore.innerHTML = `Score: ${score}`;
+    alert('Bem vindo! Iniciando um novo jogo!')
+    score = 0;
+    
+    nextLevel();
+}
+
+//Eventos de clique das cores.
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+//Função de iniciar o jogo.
+playGame();
